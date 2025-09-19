4834292cbcb1c32b844e16c7bfa955e07fb4df9e
cmd-mvp <murilogrecchi@hotmail.com>
2025-09-19 16:03:59 -0300

    Update RentDto.cs

--- Car Manager/Data/Dtos/RentDto.cs
+++ Car Manager/Data/Dtos/RentDto.cs
@@ -9,7 +9,6 @@ namespace Car_Manager.Data.Dtos
         public int Id { get; set; }
         public int CarId { get; set; }
         public string UserName { get; set; }
-        //public string UsersCPF { get; set; }
         public DateTime RentDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public string Status { get; set; }
